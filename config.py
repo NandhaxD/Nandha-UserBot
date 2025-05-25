0bce67a17a67425ab81343d8d9438a4c9d77175c
Nandhagopal <89440790+NandhaxD@users.noreply.github.com>
2025-05-25 08:44:42 -0300

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,8 @@ API_HASH = getenv('API_HASH', "eb06d4abfb49dc3eeb1aeb98ae0f581e")
 S_STRING = getenv('S_STRING', "...")
 GIST_TOKEN = getenv('GIST', "...")
 
+PORT = int(getenv("PORT", 8080))
+BIND_ADDRESS = str(getenv("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
+
 HELP = {}
 HELP_COLUMNS = 4
