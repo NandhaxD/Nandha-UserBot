73378ced268aba3f55cb2a6ad754c99182c543f6
Nandhagopal <89440790+NandhaxD@users.noreply.github.com>
2025-05-25 08:45:41 -0300

    Update __main__.py

--- nandha/__main__.py
+++ nandha/__main__.py
@@ -1,17 +1,40 @@
 import asyncio
+import pyrogram 
+import config 
 
-from nandha import app
-from pyrogram import idle
+from nandha import app, log
 from strings import RESTART_TEXT
+from web import keep_alive, web_server
+from aiohttp import web
 
+
+async def start_services():        
+        server = web.AppRunner(web_server())
+        await server.setup()
+        await web.TCPSite(server, config.BIND_ADDRESS, config.PORT).start()
+        log.info("Web Server Initialized Successfully")
+        log.info("=========== Service Startup Complete ===========")
+
+        asyncio.create_task(keep_alive())
+        log.info("Keep Alive Service Started")
+        log.info("=========== Initializing Web Server ===========")
+     
 async def main():
+     log.info('PYROGRAM HAS BEEN STARTED!')
+     await start_services()
      await app.start()
      await app.send_message('me', text=RESTART_TEXT)
-     await idle()
-  
+     await pyrogram.idle()
+
+
+
+
+
+
+        
 if __name__ == "__main__":
-     loop = asyncio.get_event_loop()
-     loop.run_until_complete(main())
+     asyncio.get_event_loop().run_until_complete(main())
+     
   
   
     
